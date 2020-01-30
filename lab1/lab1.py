b84bf2193fa299b6999dd0ba6466981ddd9e44af
satturn <satturn@bitbucket.org>
2020-01-30 01:06:11 +0000

    bug fixes

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -23,16 +23,16 @@ def is_invertible(A_inv: np.array, A_cap: np.array, x: np.array, i: int) -> bool
     A_cap = A_cap.copy()
     x = x.copy()
 
-    l = apply_mat(A_inv, x)
-    if (l[i] == 0):
+    l = A_inv @ x
+    if (l[i, 0] == 0):
         return False
     else:
         l_wave = l.copy()
-        l_wave[i] = -1
+        l_wave[i, 0] = -1
         l_caret = l_wave/(-l[i])
 
-        Q = np.identity(len(A_cap)) #attention
-        Q[:, i] = l_caret
+        Q = np.identity(len(A_cap))
+        Q[:, i] = l_caret[:, 0]
 
         A_cap_inv = Q @ A_inv
         print(f"inversed A_cap matrix:\n{A_cap_inv}")
